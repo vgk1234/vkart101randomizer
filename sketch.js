befb710b76bc45f606e31aa0dccce17f57aee012
vgk1234 <vgk1234@github.io>
2020-09-26 13:57:59 -0700

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -20,16 +20,7 @@ function setup() {
   createCanvas(600, 600);
   background(200);
 
-  console.log("initial array is ");
-  console.log(icecream);
-
-  icecream.shift(2, 2);
-  console.log("array after shift")
-  console.log(icecream);
-
-  icecream.unshift("doublescoop");
-  console.log("array after unshift")
-  console.log(icecream);
+  console.log(dogs[0].name);
 
 }
 
