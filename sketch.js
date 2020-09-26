5e810fb4c3599f0f6fc5956c1f7a056dd6cf250a
vgk1234 <vgk1234@github.io>
2020-09-26 12:22:28 -0700

    Updated Randomizer Sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
+let icecream = ["vanilla","chocolate", "mint", "bubblegum"];
+
+
 function setup() {
   createCanvas(600, 600);
   background(200);
+
+  console.log(icecream[0]);
+
 }
 
 function draw() {
