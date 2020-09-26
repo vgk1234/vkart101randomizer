3a7b78e6cfa2dd45ddbbd3dd24978a9ae2e1acf8
vgk1234 <vgk1234@github.io>
2020-09-26 13:47:03 -0700

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -5,13 +5,16 @@ function setup() {
   createCanvas(600, 600);
   background(200);
 
-  console.log("initial array is");
+  console.log("initial array is" + icecream);
   console.log(icecream);
 
-  icecream.splice(2, 2);
-  console.log("array after splice")
+  icecream.shift(2, 2);
+  console.log("array after shift")
   console.log(icecream);
 
+  icecream.unshift("doublescoop");
+  console.log("array after unshift")
+  console.log(icecream);
 
 }
 
