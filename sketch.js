b6f062dddcea297f1cff9c502bffa576b25977c2
vgk1234 <vgk1234@github.io>
2020-10-01 16:21:09 -0700

    Updated Randomizer Sketch

--- sketch.js
+++ sketch.js
@@ -30,6 +30,12 @@ function setup() {
   createCanvas(600, 600);
   background(200);
 
+  textSize(32);
+
+  text("randomize", 200, 200);
+    
+  console.log(icecream[0].name);
+
   console.log("initial array is ")
   console.log(icecream);
 
@@ -43,6 +49,36 @@ function setup() {
 
 }
 
-function draw() {
-
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+    textSize(32);
+    
+    text("randomize", 200, 200);
+    
+    setInterval(changeBackground, 1000);
+    
+    console.log(icecream[0].name);
+  
+  }
+  
+function draw () {
+    if (animating == true) {
+      ellipse(random(width), random (height), random(50, 200));
+    }
+  }
+  
+function changeBackground () {
+      background (random(255), random(255), random(255));
+  }
+  
+function randomizer() {
+      background(random (200, 255));
+      randomIndex = int(random(icecream.length));  
+      text(icecream[randomIndex].name, 200, 200);
+      icecream.splice(randomIndex, 1);
 }
+  
+function mousePressed() {
+      setTimeout(randomizer, 50);
+  }
