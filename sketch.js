b272d8f89ef29d81a0676103adbfc18f3e057ad1
vgk1234 <vgk1234@github.io>
2020-10-07 13:47:55 -0700

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -4,19 +4,19 @@ let IceCream = [];
 let imageCounter = 0;
 
 function preload () {
- for (let i = 0; i <= 4; i++) {
+ for (let i = 0; i < 5; i++) {
    IceCream[i] = loadImage(`Assets/Resized_Images/IceCream_${i}.jpg`)
  }
 }
 
 function setup() {
-  text("Click Here", 0, 0);
   createCanvas(400, 400);
   background(200);
   frameRate(12);
-  text("Click Here", 0, 0);
+  textSize(32);
+  text("Click Here", 100, 100);
 
-  button = createButton('RANDOMIZE');
+  button = createButton("RANDOMIZE");
   //button.position(450, 500)
   button.mousePressed();
 
@@ -29,14 +29,16 @@ function mousePressed() {
     if (animating == true) {
       clear();
       image(IceCream[imageCounter], 0, 0);
-      if (imageCounter < IceCream.length - 1) {
+      if (imageCounter < IceCream.length + 1) {
       imageCounter++;
       console.log(imageCounter);
       }
       else {
         imageCounter = 0;
+        createCanvas(400, 400);
         background(200);
-        text('NO MORE ITEMS');
+        textSize(32);
+        text("NO MORE", 100, 100);
       }
     }
 }
